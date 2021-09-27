1761d9816629ba64f3c156c869d9b460aede3325
QuangTung97 <quangtung29121997@gmail.com>
2021-09-27 11:03:56 +0700

    Add Cache

--- bigcache.go
+++ bigcache.go
@@ -1,1 +1,85 @@
 package bigcache
+
+import (
+	"github.com/QuangTung97/bigcache/memhash"
+	"math/bits"
+)
+
+// Cache ...
+type Cache struct {
+	segments []segment
+
+	segmentMask  uint64
+	segmentShift int
+}
+
+// New ...
+func New(numSegments int, segmentSize int) *Cache {
+	if numSegments < 1 {
+		panic("numSegments must not be < 1")
+	}
+	numSegments = nextPowerOfTwo(numSegments)
+
+	segments := make([]segment, numSegments)
+	for i := range segments {
+		initSegment(&segments[i], segmentSize)
+	}
+
+	mask, shift := computeSegmentMask(numSegments)
+	return &Cache{
+		segments:     segments,
+		segmentMask:  mask,
+		segmentShift: shift,
+	}
+}
+
+func (c *Cache) getSegment(key []byte) (*segment, uint64) {
+	hash := memhash.Hash(key)
+	index := getSegmentIndex(c.segmentMask, c.segmentShift, hash)
+	return &c.segments[index], hash
+}
+
+// Put ...
+func (c *Cache) Put(key []byte, value []byte) {
+	seg, hash := c.getSegment(key)
+
+	seg.mu.Lock()
+	seg.put(uint32(hash), key, value)
+	seg.mu.Unlock()
+}
+
+// Get ...
+func (c *Cache) Get(key []byte, value []byte) (int, bool) {
+	seg, hash := c.getSegment(key)
+
+	seg.mu.Lock()
+	n, ok := seg.get(uint32(hash), key, value)
+	seg.mu.Unlock()
+
+	return n, ok
+}
+
+// Delete ...
+func (c *Cache) Delete(key []byte) bool {
+	seg, hash := c.getSegment(key)
+
+	seg.mu.Lock()
+	affected := seg.delete(uint32(hash), key)
+	seg.mu.Unlock()
+
+	return affected
+}
+
+func nextPowerOfTwo(n int) int {
+	num := uint32(n)
+	return 1 << bits.Len32(num-1)
+}
+
+func computeSegmentMask(n int) (uint64, int) {
+	shift := 64 - bits.Len64(uint64(n-1))
+	return ^uint64(0) << shift, shift
+}
+
+func getSegmentIndex(mask uint64, shift int, hash uint64) int {
+	return int((hash & mask) >> shift)
+}
---
+++ bigcache_test.go
@@ -0,0 +1,69 @@
+package bigcache
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNextPowerOfTwo(t *testing.T) {
+	assert.Equal(t, 1, nextPowerOfTwo(1))
+	assert.Equal(t, 2, nextPowerOfTwo(2))
+	assert.Equal(t, 4, nextPowerOfTwo(3))
+	assert.Equal(t, 16, nextPowerOfTwo(15))
+	assert.Equal(t, 16, nextPowerOfTwo(12))
+	assert.Equal(t, 16, nextPowerOfTwo(16))
+	assert.Equal(t, 64, nextPowerOfTwo(59))
+}
+
+func TestGetSegmentIndex(t *testing.T) {
+	mask, shift := computeSegmentMask(1)
+	assert.Equal(t, uint64(0x0), mask)
+	assert.Equal(t, 64, shift)
+
+	mask, shift = computeSegmentMask(16)
+	assert.Equal(t, uint64(0xf<<60), mask)
+	assert.Equal(t, 60, shift)
+
+	assert.Equal(t, 0, getSegmentIndex(0xf<<60, 60, 0x0033445500000000))
+	assert.Equal(t, 2, getSegmentIndex(0xf<<60, 60, 0x2033445500000000))
+
+	mask, shift = computeSegmentMask(64)
+	assert.Equal(t, uint64(0xfc<<56), mask)
+	assert.Equal(t, 58, shift)
+
+	assert.Equal(t, 0b010110, getSegmentIndex(mask, shift, 0x5933445500000000))
+}
+
+func TestCache(t *testing.T) {
+	c := New(3, 12345)
+
+	c.Put([]byte{10, 11, 12}, []byte{20, 21, 22, 20})
+	c.Put([]byte{10, 11, 13}, []byte{20, 21, 22, 21})
+	c.Put([]byte{10, 11, 14}, []byte{20, 21, 22, 22})
+
+	value := make([]byte, 20)
+
+	n, ok := c.Get([]byte{10, 11, 12}, value)
+	assert.Equal(t, true, ok)
+	assert.Equal(t, []byte{20, 21, 22, 20}, value[:n])
+
+	n, ok = c.Get([]byte{10, 11, 15}, value)
+	assert.Equal(t, false, ok)
+	assert.Equal(t, []byte{}, value[:n])
+
+	ok = c.Delete([]byte{10, 11, 12})
+	assert.Equal(t, true, ok)
+
+	ok = c.Delete([]byte{10, 11, 12})
+	assert.Equal(t, false, ok)
+
+	n, ok = c.Get([]byte{10, 11, 12}, value)
+	assert.Equal(t, false, ok)
+	assert.Equal(t, []byte{}, value[:n])
+}
+
+func TestCache_New_Panic(t *testing.T) {
+	assert.PanicsWithValue(t, "numSegments must not be < 1", func() {
+		New(0, 12345)
+	})
+}
